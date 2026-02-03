6bf30937079c6015b25339ac54ea9a643daf673b
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-03 17:19:42 +0300

    feat: добавлен alimbic

--- src/lms/infrastructure/database/session.py
+++ src/lms/infrastructure/database/session.py
@@ -1,14 +1,18 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/lms"
+ASYNC_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/lms"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(ASYNC_DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
+
+SYNC_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/lms"
+sync_engine = create_engine(SYNC_DATABASE_URL, echo=False)
 
 Base = declarative_base()
